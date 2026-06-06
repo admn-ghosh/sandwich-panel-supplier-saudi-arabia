6eb3970c5da0d0166136c2e308e413cb8120b3de
admn-ghosh <admin@ghoshgroups.com>
2026-06-06 12:59:29 +0000

    Deploying to gh-pages from @ admn-ghosh/sandwich-panel-supplier-saudi-arabia@485cbd5ee719a6895a3ad2b5316bc3b5225f7c21 🚀

--- assets/quote-w0Il4Gt7.js
+++ assets/quote-w0Il4Gt7.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CNqJ-soB.js";/**
+import{c as a}from"./index-D4SmVUYR.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/truck-Bi6cm9Qz.js
+++ assets/truck-Bi6cm9Qz.js
@@ -1,4 +1,4 @@
-import{c}from"./index-CNqJ-soB.js";/**
+import{c}from"./index-D4SmVUYR.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
